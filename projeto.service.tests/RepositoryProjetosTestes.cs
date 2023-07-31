8b1049dda49dd935cc56d601cc64293cbeb66243
k4im <contato.vitorsantos@hotmail.com>
2023-07-31 13:19:56 -0300

    Criado testes de repository

--- projeto.service.tests/RepositoryProjetosTestes.cs
+++ projeto.service.tests/RepositoryProjetosTestes.cs
@@ -31,5 +31,93 @@ namespace projeto.service.tests
             //Assert
             Assert.Equal(result, resposta);
         }
+
+        [Fact]
+        public async void deve_retornar_true_ao_adicionar_projeto()
+        {
+            //Arrange
+            var projeto = FakeProjeto.factoryProjeto();
+            mockRepo.Setup(repo => repo.CriarProjeto(projeto).Result)
+            .Returns(true);
+
+            // Act
+            var result = await mockRepo.Object.CriarProjeto(projeto);
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async void deve_retornar_true_ao_atualizar_projeto()
+        {
+            //Arrange
+            var projetoStatus = FakeStatus.factoryStatus();
+            mockRepo.Setup(repo => repo.AtualizarStatus(projetoStatus, 1).Result)
+            .Returns(true);
+
+            //Act
+            var result = await mockRepo.Object.AtualizarStatus(projetoStatus, 1);
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async void deve_retornar_true_ao_deletar_projeto()
+        {
+            //Arrange
+            mockRepo.Setup(repo => repo.DeletarProjeto(1).Result)
+            .Returns(true);
+
+            //Act
+            var result = await mockRepo.Object.DeletarProjeto(1);
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async void deve_retornar_false_ao_adicionar_projeto()
+        {
+            // Arrange
+            var projeto = FakeProjeto.factoryProjeto();
+            mockRepo.Setup(repo => repo.CriarProjeto(projeto).Result)
+            .Returns(false);
+
+            // Act
+            var result = await mockRepo.Object.CriarProjeto(projeto);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async void deve_retornar_false_ao_atualizar_projeto()
+        {
+            //Arrange
+            var projetoStatus = FakeStatus.factoryStatus();
+            mockRepo.Setup(repo => repo.AtualizarStatus(projetoStatus, 1).Result)
+            .Returns(false);
+
+            //Act
+            var result = await mockRepo.Object.AtualizarStatus(projetoStatus, 1);
+
+            //Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public async void deve_retornar_false_ao_deletar_projeto()
+        {
+            // Arrange
+            mockRepo.Setup(repo => repo.DeletarProjeto(1).Result)
+            .Returns(false);
+
+            // Act
+            var result = await mockRepo.Object.DeletarProjeto(1);
+
+            // Assert
+            Assert.False(result);
+        }
     }
 }
