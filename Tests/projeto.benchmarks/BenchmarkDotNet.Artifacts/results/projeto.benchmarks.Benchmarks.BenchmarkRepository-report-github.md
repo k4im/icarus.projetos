9b2d4650aa4d51c74d8cb8b7433fed90e4b437d7
k4im <contato.vitorsantos@hotmail.com>
2023-08-03 14:16:29 -0300

    adicionado pagination dapper

--- Tests/projeto.benchmarks/BenchmarkDotNet.Artifacts/results/projeto.benchmarks.Benchmarks.BenchmarkRepository-report-github.md
+++ Tests/projeto.benchmarks/BenchmarkDotNet.Artifacts/results/projeto.benchmarks.Benchmarks.BenchmarkRepository-report-github.md
@@ -3,15 +3,18 @@
 BenchmarkDotNet v0.13.6, Linux Mint 21.1 (Vera)
 Intel Core i3-3240 CPU 3.40GHz (Ivy Bridge), 1 CPU, 4 logical and 2 physical cores
 .NET SDK 7.0.109
-  [Host]   : .NET 7.0.9 (7.0.923.32301), X64 RyuJIT AVX
-  ShortRun : .NET 7.0.9 (7.0.923.32301), X64 RyuJIT AVX
+  [Host]  : .NET 7.0.9 (7.0.923.32301), X64 RyuJIT AVX
+  LongRun : .NET 7.0.9 (7.0.923.32301), X64 RyuJIT AVX
 
-Job=ShortRun  IterationCount=3  LaunchCount=1  
-WarmupCount=3  
+Job=LongRun  IterationCount=100  LaunchCount=3  
+WarmupCount=15  
 
 ```
-|                           Method |      Mean |      Error |    StdDev | Rank |    Gen0 | Allocated |
-|--------------------------------- |----------:|-----------:|----------:|-----:|--------:|----------:|
-|          BuscarProdutosPaginados | 483.83 μs |  25.120 μs |  1.377 μs |    2 | 52.7344 |  81.23 KB |
-|       BuscarProdutosPaginadosSql | 739.73 μs | 473.192 μs | 25.937 μs |    3 | 66.4063 | 103.47 KB |
-| BuscarProdutosPaginadosSqlDapper |  59.86 μs |   6.366 μs |  0.349 μs |    1 |  3.4180 |   5.38 KB |
+|                           Method | interation |      Mean |    Error |   StdDev |    Median | Rank |    Gen0 | Allocated |
+|--------------------------------- |----------- |----------:|---------:|---------:|----------:|-----:|--------:|----------:|
+|          **BuscarProdutosPaginados** |       **1000** | **442.37 μs** | **1.181 μs** | **5.854 μs** | **441.06 μs** |    **3** | **52.7344** |  **81.23 KB** |
+|       BuscarProdutosPaginadosSql |       1000 | 585.17 μs | 1.702 μs | 8.514 μs | 583.53 μs |    4 | 64.4531 |  99.89 KB |
+| BuscarProdutosPaginadosSqlDapper |       1000 |  54.75 μs | 0.100 μs | 0.499 μs |  54.76 μs |    1 |  3.1128 |   4.82 KB |
+|          **BuscarProdutosPaginados** |       **2000** | **438.47 μs** | **0.871 μs** | **4.408 μs** | **437.74 μs** |    **3** | **52.7344** |  **81.23 KB** |
+|       BuscarProdutosPaginadosSql |       2000 | 587.20 μs | 1.157 μs | 5.703 μs | 587.47 μs |    4 | 64.4531 |  99.89 KB |
+| BuscarProdutosPaginadosSqlDapper |       2000 |  57.84 μs | 0.574 μs | 2.871 μs |  57.05 μs |    2 |  3.1128 |   4.82 KB |
--- Tests/projeto.benchmarks/Benchmarks/BenchmarkRepository.cs
+++ Tests/projeto.benchmarks/Benchmarks/BenchmarkRepository.cs
@@ -3,58 +3,40 @@ using Microsoft.Data.Sqlite;
 
 namespace projeto.benchmarks.Benchmarks;
 
-[MemoryDiagnoser, RankColumn, ShortRunJob]
+[MemoryDiagnoser, RankColumn, LongRunJob]
 public class BenchmarkRepository
 {
+
     [Params(1_000, 2_000)]
-    public int Interations;
-
-    // [GlobalSetup]
-    // public async void Setup()
-    // {
-    //     var connection = new SqliteConnection("Data Source=:memory:");
-    //     await connection.OpenAsync();
-    //     _context = new DataContext(new DbContextOptionsBuilder().UseSqlite(connection).Options);
-    //     _context.Database.EnsureCreated();
-    //     for (int i = 0; i < numerosDeInteracao; i++)
-    //         _context.Projetos.AddRange(FakeProjeto.factoryProjeto());
-    //     await _context.SaveChangesAsync();
-    // }
-
+    public int interation;
     [Benchmark]
     public async Task<Response<Projeto>> BuscarProdutosPaginados()
     {
-        using (var db = new DataContext())
-        {
-            var pessoas = await db.Projetos.ToListAsync();
-            var totalDePaginas = Math.Ceiling(pessoas.Count() / 10f);
-            var produtosPaginados = pessoas.Skip((1 - 1) * (int)10).Take((int)10).ToList();
-            var paginasTotal = (int)totalDePaginas;
-            return new Response<Projeto>(produtosPaginados, 1, (int)totalDePaginas);
-        };
+        using var db = new DataContext();
+        var pessoas = await db.Projetos.ToListAsync();
+        var totalDePaginas = Math.Ceiling(pessoas.Count() / 10f);
+        var produtosPaginados = pessoas.Skip((1 - 1) * (int)10).Take((int)10).ToList();
+        var paginasTotal = (int)totalDePaginas;
+        return new Response<Projeto>(produtosPaginados, 1, (int)totalDePaginas);
     }
 
 
     [Benchmark]
     public async Task<Response<Projeto>> BuscarProdutosPaginadosSql()
     {
-        using (var context = new DataContext())
-        {
-            var total = await context.Projetos.FromSql($"SELECT COUNT(*) FROM Projetos").CountAsync();
-            var projetosPaginados = await context.Projetos.FromSql($"SELECT * FROM Projetos LIMIT 10 OFFSET 0").ToListAsync();
-            return new Response<Projeto>(projetosPaginados, 1, total);
-        }
+        using var context = new DataContext();
+        var total = await context.Projetos.FromSql($"SELECT COUNT(*) FROM Projetos").CountAsync();
+        var projetosPaginados = await context.Projetos.FromSql($"SELECT * FROM Projetos LIMIT 10 OFFSET 0").ToListAsync();
+        return new Response<Projeto>(projetosPaginados, 1, total);
     }
 
     [Benchmark]
     public async Task<Response<Projeto>> BuscarProdutosPaginadosSqlDapper()
     {
-        using (var context = new SqliteConnection("Data Source=teste.db;"))
-        {
-            var total = context.Query($"SELECT COUNT(*) FROM Projetos").Count();
-            var query = $"SELECT * FROM Projetos LIMIT 10 OFFSET 0";
-            var projetos = await context.QueryAsync<Projeto>(query);
-            return new Response<Projeto>(projetos.ToList(), 1, total);
-        }
+        using var context = new SqliteConnection("Data Source=teste.db;");
+        var total = context.ExecuteScalar<int>($"SELECT COUNT(*) FROM Projetos");
+        var query = $"SELECT * FROM Projetos LIMIT 10 OFFSET 0";
+        var projetos = await context.QueryAsync<Projeto>(query);
+        return new Response<Projeto>(projetos.ToList(), 1, total);
     }
 }
