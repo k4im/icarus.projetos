b4d9f0a79c0c275e59c6f92d56a9dfe3ccb2aeee
joao <joao@platon.com.br>
2023-08-25 15:34:10 -0300

    Adicionado fix do produtos

--- projeto.domain/Dtos/ProdutoEmEstoqueDTO.cs
+++ projeto.domain/Dtos/ProdutoEmEstoqueDTO.cs
@@ -2,7 +2,7 @@ namespace projeto.domain.Dtos;
 
 public class ProdutoEmEstoqueDTO
 {
-    public string Id { get; set;}
+    public int Id { get; set;}
     public string Nome { get; set; }
     public int Quantidade { get; set; }
 }
