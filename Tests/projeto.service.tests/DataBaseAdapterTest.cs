596bfcc514315559fea4fccb7fd349874c016886
joao <joao@platon.com.br>
2023-11-29 10:16:38 -0300

    Adicionado testes para Adapter

--- Tests/projeto.service.tests/DataBaseAdapterTest.cs
+++ Tests/projeto.service.tests/DataBaseAdapterTest.cs
@@ -1,14 +1,16 @@
 using projeto.infra.Adapters.DatabaseAdapter;
 using projeto.infra.Adapters.DatabasePort;
+using Xunit.Sdk;
 
 namespace projeto.service.tests;
 public class DataBaseAdapterTest
 {
+    DatabaseAdapterProjeto Adapter = new DatabaseAdapterProjeto(new DataBaseConnectionSqlite());
+    
     [Fact]
     public async void Deve_realizar_operacao_select_no_banco()
     {
         //Arrange
-        var Adapter = new DatabaseAdapterProjeto(new DataBaseConnectionSqlite());
         var queryPaginado = @"
         SELECT 
             Id, 
@@ -20,15 +22,70 @@ public class DataBaseAdapterTest
         FROM 
             Projetos 
         LIMIT 
-            5 
+            @resultado
         OFFSET 
-            1";
+            @pagina";
         var queryTotal = "SELECT count(*) FROM Projetos";
 
         //Act 
-        var result = await Adapter.PaginarProjetosDoBancoDapper(queryPaginado, queryTotal, 1, 5);
+        var result = await this.Adapter.PaginarProjetosDoBancoDapper(queryPaginado, queryTotal, 1, 5);
 
         //Assert
         Assert.NotNull(result);
     }
+    [Fact]
+    public void Deve_realizar_filtragem_ao_banco_de_dados()
+    {
+        //Arrange
+        var queryPaginado = @"
+        SELECT 
+            Id, 
+            Nome, 
+            Status, 
+            DataInicio, 
+            DataEntrega, 
+            Valor 
+        FROM 
+            Projetos 
+        WHERE
+            Status 
+        LIKE
+            @filter   
+        LIMIT 
+            @resultado 
+        OFFSET 
+            @pagina";
+        var queryTotal = "SELECT COUNT(*) FROM Projetos WHERE Status LIKE @filter OR Nome LIKE @filter";
+        
+        // Act
+        var result = this.Adapter.FiltrarDadosPaginadosDoBancoDapper(queryPaginado, "teste", queryTotal, 1, 5);
+        
+        // Then
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public void Deve_buscar_dados_pelo_id()
+    {
+        // Given
+        var query = @"
+            SELECT 
+                Projetos.*,
+                ProdutosEmEstoque.*
+            FROM 
+                Projetos 
+            INNER JOIN 
+                ProdutosEmEstoque
+            ON 
+                Projetos.ProdutoUtilizadoId = ProdutosEmEstoque.Id
+            WHERE 
+                Projetos.Id 
+            LIKE 
+                @busca";
+        // When
+         var result = this.Adapter.BuscarDadosDoBancoPeloId(1, query);
+        
+        // Then
+        Assert.NotNull(result);
+    }   
 }
--- Tests/projeto.service.tests/RepositoryProjetosTestes.cs
+++ Tests/projeto.service.tests/RepositoryProjetosTestes.cs
@@ -3,35 +3,7 @@ namespace projeto.service.tests;
 public class RepositoryProjetosTestes
 {
     Mock<IRepoProjetos> mockRepo = new Mock<IRepoProjetos>();
-    [Fact]
-    public async void Deve_retornar_lista_de_projetos()
-    {
-        //Arrange
-        var resposta = new Response<ProjetoPaginadoDTO>(FakeProjetoPaginado.FactoryListaProjetos(), 1, 1, 1);
-        mockRepo.Setup(repo => repo.BuscarProdutos(1, 5).Result)
-        .Returns(resposta);
-        //Act
-        var result = await mockRepo.Object.BuscarProdutos(1, 5);
-
-        //Assert
-        Assert.Equal(result, resposta);
-    }
-
-    [Fact]
-    public async void Deve_retonar_projeto()
-    {
-        // Arrange
-        var resposta = FakeProjetoBuscaId.FactoryProjeto();
-        mockRepo.Setup(repo => repo.BuscarPorId(1).Result)
-        .Returns(resposta);
-
-        //Act
-        var result = await mockRepo.Object.BuscarPorId(1);
-
-        //Assert
-        Assert.Equal(result, resposta);
-    }
-
+    
     [Fact]
     public async void Deve_retornar_true_ao_adicionar_projeto()
     {
