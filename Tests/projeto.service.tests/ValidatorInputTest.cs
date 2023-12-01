b8766e8287c2bd3654c80eaabf5167b808cf55f7
joao <joao@platon.com.br>
2023-12-01 13:48:37 -0300

    Adicionado um teste

---
+++ Tests/projeto.service.tests/ValidatorInputTest.cs
@@ -0,0 +1,51 @@
+using projeto.domain.Validators;
+
+namespace projeto.service.tests;
+public class ValidatorInputTest
+{
+    [Fact]
+    public void Deve_retornar_false_no_validador_menor_que_zero()
+    {
+        //Arrange
+        var ValorMenorQueZero = -1;
+        var ValorIgualAZero = 0;
+        var ValorMaiorQueZero = 1;
+    
+        // Act
+        var ResultMenorQueZero = ValidadorDeInput.ValidarMenorDoQueZero(ValorMenorQueZero);
+        var ResultIgualAZero = ValidadorDeInput.ValidarMenorDoQueZero(ValorIgualAZero);
+        var ResultMaiorQueZero = ValidadorDeInput.ValidarMenorDoQueZero(ValorMaiorQueZero);
+
+        //Assert
+        Assert.True(ResultMenorQueZero);
+        Assert.False(ResultIgualAZero);
+        Assert.False(ResultMaiorQueZero);
+    }
+
+    [Fact]
+    public void Deve_retornar_false_validador_regex()
+    {
+        // Arrange
+        var valor = "Teste de Regex Onde é Enviado Valores 4ltet0ri05";
+        
+        // Acts
+        var result = ValidadorDeInput.ValidarInputRegex(valor);
+        
+        // Assert
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void Deve_retornar_true_validador_regex()
+    {
+        // Arrange
+        var valor = "Teste de Regex@@@ Onde é Enviado Valores 4ltet0ri05";
+        
+        // Acts
+        var result = ValidadorDeInput.ValidarInputRegex(valor);
+        
+        // Assert
+        Assert.True(result);
+    }
+}
