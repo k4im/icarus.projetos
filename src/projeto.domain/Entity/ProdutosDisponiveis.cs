8a8338329f3dbe2761a712562c53e7f118a0110e
joao <joao@platon.com.br>
2024-01-08 10:02:01 -0300

    Removido classes

--- src/projeto.domain/Entity/ProdutosDisponiveis.cs
+++
@@ -1,9 +0,0 @@
-namespace projeto.domain.Entity;
-
-public class ProdutosDisponiveis
-{
-    public int Id { get; set; }
-    public string Nome { get; set; }
-    public int Quantidade { get; set; }
-    public ICollection<Projeto> Projetos { get; set; } = new List<Projeto>();
-}
